3ba536882fe33b30a9dc85aa3306343247daaf06
Makuo67 <okekemakuo67@gmail.com>
2023-02-20 23:44:34 +0200

    :robot: automated commit of one or many files

--- models/state.py
+++ models/state.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from models.city import City
+from sqlalchemy import Column, Integer, String
 import models
-import os
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    # cities = relationship("City", cascade="delete")
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City', backref='state', cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """Returns cities obj
-            """
-            newlist = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    newlist.append(city)
-            return (newlist)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-
     """This is the class for user
     Attributes:
         email: email address
@@ -14,12 +16,12 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship(
-        "Place", backref='user', cascade='all, delete-orphan')
-    reviews = relationship(
-        "Review", backref='user', cascade='all, delete-orphan')
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
