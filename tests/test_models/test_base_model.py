fbd21d2e865bbcd1eaa30603472a6603325e6311
Makuo67 <okekemakuo67@gmail.com>
2023-02-22 23:52:43 +0200

    Basemodel correction

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,7 @@ class TestBaseModel(unittest.TestCase):
         """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
-        self.value = BaseModel()
+        self.value = BaseModel
 
     def setUp(self):
         """ """
