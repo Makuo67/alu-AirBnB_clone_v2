8a885abb01f8685b4752cd61a33ef5efe51322da
Makuo67 <okekemakuo67@gmail.com>
2023-02-22 02:01:56 +0200

    :robot: automated commit of one or many files

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,7 +9,6 @@ class test_User(test_basemodel):
 
     def __init__(self, *args, **kwargs):
         """ """
-        super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
