3f7c014e80c9feffbaece8965c0f890688089e87
Makuo67 <okekemakuo67@gmail.com>
2023-02-23 04:01:43 +0200

    db storage errors check

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,17 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declared_attr
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
     # used for db storage
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
