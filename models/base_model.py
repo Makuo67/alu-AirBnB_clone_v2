cc3444a44bc88bc26c461fa51303b45ed74224bc
Makuo67 <okekemakuo67@gmail.com>
2023-02-23 02:20:28 +0200

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -44,7 +44,7 @@ class BaseModel:
 
             if '__class__' in kwargs:
                 del kwargs['__class__']
-        self.__dict__.update(kwargs)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
