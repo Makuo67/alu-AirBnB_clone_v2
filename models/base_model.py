4376ed27664fb9d849e6792d33e2c3954f3ac74d
Makuo67 <okekemakuo67@gmail.com>
2023-02-23 04:07:33 +0200

    :robot: automated commit of one or many files

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,15 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declared_attr
+
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
     # used for db storage
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
