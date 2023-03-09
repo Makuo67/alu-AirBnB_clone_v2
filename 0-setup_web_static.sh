cad2460b4330209874c0fe1fd71ff3d6bccb1657
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-03-09 20:06:16 +0200

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,9 @@ apt-get -y install nginx
 service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "ALU Webstatic Project" > /data/web_static/releases/test/index.html
+echo "<html><head><title>Test</title></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '33i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
