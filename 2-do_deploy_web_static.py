9f896e335d9b9a872837d8e452719a1c1cf0f69f
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-03-09 21:18:41 +0200

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,30 +11,29 @@ env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ Deploys """
+    """Distributes an archive to your web servers"""
     if not os.path.exists(archive_path):
         return False
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
+        archive_filename = os.path.basename(archive_path)
+        archive_name = os.path.splitext(archive_filename)[0]
+        remote_path = '/data/web_static/releases/{}'.format(archive_name)
+        run('sudo mkdir -p {}'.format(remote_path))
+        run('sudo tar -xzf /tmp/{} -C {} --strip-components=1'.format(archive_filename, remote_path))
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
+        # Delete the archive from the web server
+        run('sudo rm /tmp/{}'.format(archive_filename))
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run('sudo rm -f /data/web_static/current')
+
+        # Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
+        run('sudo ln -s {} /data/web_static/current'.format(remote_path))
+
         return True
     except:
         return False
