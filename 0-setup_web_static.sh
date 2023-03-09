f5d80faf392d5c261f1766968be45f86c2a82ec4
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2023-03-09 20:08:30 +0200

    set-up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+
+# Install Nginx if it's not already installed
+if ! which nginx > /dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories if they don't already exist
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo chown -R ubuntu:ubuntu /data/
+
+# Create a fake HTML file for testing purposes
 echo "<html><head><title>Test</title></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+
+# Create symbolic link and update Nginx configuration
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo sed -i '33i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
+
+# Restart Nginx
+sudo service nginx restart
 exit 0
