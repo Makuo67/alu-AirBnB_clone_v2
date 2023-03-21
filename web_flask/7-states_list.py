53e157da706cb0b5d643ec0b858c86f814b46a4e
Makuo67 <okekemakuo67@gmail.com>
2023-03-22 01:51:17 +0300

    ok

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """Comment"""
+    """Comment
+    What else does this wants"""
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
 
